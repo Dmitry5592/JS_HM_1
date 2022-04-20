7ecc371896aa936436a5b50d049b4de1b1156bb5
Dmitry Kurtyigin <103406463+Dmitry5592@users.noreply.github.com>
2022-04-20 18:19:04 +0600

    HW Js lesson 1, tasks with an asterisk

---
+++ HW_1_Js_1.js
@@ -0,0 +1,17 @@
+function checkAge(age){
+
+
+if(age < 18) {
+    console.log(`"You don't have access cause your age is"  age  "It's less then"`)
+} 
+    else if(age >= 18 && age < 60) {
+    console.log("Welcome   !")
+ }
+    else if(age > 60) {
+        console.log ("Keep calm and look Culture channel")
+    }
+    else {
+        console.log("Technical work")
+    }
+}
+checkAge (14)
